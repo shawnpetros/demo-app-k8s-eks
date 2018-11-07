f9cb08e737011d0ad1bb3295e23cc8a29c87555a
Shawn Petros <shawnpetros@northwesternmutual.com>
2018-11-06 20:27:28 -0600

    create eks cluster

---
+++ infra/eks-cluster.tf
@@ -0,0 +1,54 @@
+#
+# main EKS terraform resource definition
+#
+resource "aws_eks_cluster" "eks-cluster" {
+  name = "${var.cluster-name}"
+
+  role_arn = "${aws_iam_role.sp-cluster.arn}"
+
+  vpc_config {
+    security_group_ids = ["${aws_security_group.sp-cluster.id}"]
+    subnet_ids         = ["${module.eks-vpc.public_subnets}"]
+  }
+}
+
+locals {
+  kubeconfig = <<KUBECONFIG
+apiVersion: v1
+clusters:
+- cluster:
+    server: ${aws_eks_cluster.eks-cluster.endpoint}
+    certificate-authority-data: ${aws_eks_cluster.eks-cluster.certificate_authority.0.data}
+  name: kubernetes
+contexts:
+- context:
+    cluster: kubernetes
+    user: aws
+  name: aws
+current-context: aws
+kind: Config
+preferences: {}
+users:
+- name: aws
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1alpha1
+      command: aws-iam-authenticator
+      args:
+        - "token"
+        - "-i"
+        - "${var.cluster-name}"
+KUBECONFIG
+}
+
+output "kubeconfig" {
+  value = "${local.kubeconfig}"
+}
+
+output "endpoint" {
+  value = "${aws_eks_cluster.eks-cluster.endpoint}"
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = "${aws_eks_cluster.eks-cluster.certificate_authority.0.data}"
+}
