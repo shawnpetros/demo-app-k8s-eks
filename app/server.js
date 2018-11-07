7961025efee508dfd3356208de007917917de899
Shawn Petros <shawnpetros@northwesternmutual.com>
2018-11-07 11:07:12 -0600

    clean up and add static server

--- app/server.js
+++ app/server.js
@@ -1,13 +1,14 @@
 const express = require('express')
+const path = require('path')
 const bodyParser = require('body-parser')
 const Client = require('pg').Client
+const client = new Client()
 
 const app = express()
 const port = process.env.PORT || 5000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
-const client = new Client()
+app.use(express.static(path.resolve(__dirname, 'build')))
 
 app.get('/api/ping', async (req, res) => {
 
