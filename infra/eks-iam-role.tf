8d31e9960b1156668990eb336f62e280dc008385
Shawn Petros <shawnpetros@northwesternmutual.com>
2018-11-06 20:26:40 -0600

    create eks iam role

---
+++ infra/eks-iam-role.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_role" "sp-cluster" {
+  name = "terraform-eks-sp-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "sp-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = "${aws_iam_role.sp-cluster.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "sp-cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = "${aws_iam_role.sp-cluster.name}"
+}
