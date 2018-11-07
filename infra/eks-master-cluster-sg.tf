8477fe94f96b5c3144081c1e62b241ccb8b264ab
Shawn Petros <shawnpetros@northwesternmutual.com>
2018-11-06 21:56:17 -0600

    allow worker nodes to communicate with control plane

--- infra/eks-master-cluster-sg.tf
+++ infra/eks-master-cluster-sg.tf
@@ -24,3 +24,13 @@ resource "aws_security_group_rule" "sp-cluster-ingress-workstation-https" {
   to_port           = 443
   type              = "ingress"
 }
+
+resource "aws_security_group_rule" "sp-cluster-ingress-node-https" {
+  description              = "Allow pods to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.sp-cluster.id}"
+  source_security_group_id = "${aws_security_group.sp-node.id}"
+  to_port                  = 443
+  type                     = "ingress"
+}
