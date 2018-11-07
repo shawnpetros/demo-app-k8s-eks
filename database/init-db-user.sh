d87b6266b183107bc312577a97428686fc837541
Shawn Petros <shawnpetros@northwesternmutual.com>
2018-11-07 08:34:07 -0600

    missed a semi

--- database/init-db-user.sh
+++ database/init-db-user.sh
@@ -6,16 +6,16 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
   CREATE DATABASE docker;
   GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
   CREATE TABLE account(
-  user_id serial PRIMARY KEY,
-  username VARCHAR (50) UNIQUE NOT NULL,
-  password VARCHAR (50) NOT NULL,
-  email VARCHAR (355) UNIQUE NOT NULL,
-  created_on TIMESTAMP DEFAULT Now()
+    user_id serial PRIMARY KEY,
+    username VARCHAR (50) UNIQUE NOT NULL,
+    password VARCHAR (50) NOT NULL,
+    email VARCHAR (355) UNIQUE NOT NULL,
+    created_on TIMESTAMP DEFAULT Now()
   );
   INSERT INTO account(username, password, email)
   VALUES
     ('bob dole', 'b0b1', 'bob@bobs-code.com'),
     ('jill baker', 'p@ssw0rd', 'jill@jillsbakery.com'),
     ('jack beanstalk', 'gr33n', 'jack@giantproduce.com'),
-    ('mark webber', 'gr!ll!n', 'mark@webber-grills.com')
+    ('mark webber', 'gr!ll!n', 'mark@webber-grills.com');
 EOSQL
