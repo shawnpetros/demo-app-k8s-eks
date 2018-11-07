a04a06bce4c0aac95746313df6ed0401cfb6e7da
Shawn Petros <shawnpetros@northwesternmutual.com>
2018-11-07 12:50:40 -0600

    use pool to reuse connection - silly postgresql learning curve

--- app/server.js
+++ app/server.js
@@ -1,24 +1,22 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
-const { Client } = require('pg')
+const { Pool } = require('pg')
 
 const app = express()
 const CONN_STRING = process.env.DATABASE_URL || 'postgres://localhost:5432/'
 const port = process.env.PORT || 5000
 
-const client = new Client({ connectionString: CONN_STRING })
+const pool = new Pool({ connectionString: CONN_STRING })
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.resolve(__dirname, 'build')))
 
 app.get('/api/all', async (req, res) => {
-  await client.connect()
-  const result = await client.query('SELECT * from account')
+  const { rows } = await pool.query('SELECT * from account')
   // console.log(result.rows)
-  await client.end()
-  res.json(result.rows)
+  res.json(rows)
 })
 
 app.get('/api/ping', (req, res) => { res.send('pong') })
