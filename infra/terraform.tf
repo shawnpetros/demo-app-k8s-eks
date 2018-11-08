775e2417aafbbcd3f93e202ee7b929d045c5f1be
Shawn Petros <shawnpetros@northwesternmutual.com>
2018-11-08 13:45:53 -0600

    initialize state with aws backend

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     encrypt = true
-#     bucket = "arashcuzi-terraform-state"
-#     dynamodb_table = "terraform-state-lock-dynamo"
-#     region = "us-east-1"
-#     key = "terraform.tfstate"
-#   }
-# }
+terraform {
+  backend "s3" {
+    encrypt = true
+    bucket = "arashcuzi-terraform-state"
+    dynamodb_table = "terraform-state-lock-dynamo"
+    region = "us-east-1"
+    key = "terraform.tfstate"
+  }
+}
